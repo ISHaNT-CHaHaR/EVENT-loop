1381076429fd15e856593655e02832bd72c51024
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-01-06 15:42:19 +0530

    updated eventloop.js
    updates regarding setImmmediate and setTimeout:

--- eventloop.js
+++ eventloop.js
@@ -1,10 +1,25 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
-setTimeout(()=>console.log("timer 1 ...finished!"),0);
+setTimeout(()=>console.log("timer 1 ...finished!"),4000);
 setImmediate(()=> console.log("Immediate 1 finished!"))
 
 fs.readFile('text-file.txt',()=>{
+
+
+    setTimeout(()=>console.log("timer 2 ...finished!"),0);
+    
+    setTimeout(()=>console.log("timer 3 ...finished!"),3000);
+
+    setImmediate(()=> console.log("Immediate 2 finished!"));
+
+    process.nextTick(()=>console.log("process.nextTick")); 
+
+
     console.log('I/O finsihed!');
+    
+
+
 }
 )
 
