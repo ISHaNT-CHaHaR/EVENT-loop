4006bf6a6a870cea4e6d5ec1d6dae62ed5629b99
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-01-06 16:27:40 +0530

    updated eventloop.js
    event loops functioning can be better understood by thses codes.

--- eventloop.js
+++ eventloop.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
-setTimeout(()=>console.log("timer 1 ...finished!"),4000);
+const start= Date.now();
+
+process.env.UV_THREADPOOL_SIZE=3;
+
+setTimeout(()=>console.log("timer 1 ...finished!"),0);
 setImmediate(()=> console.log("Immediate 1 finished!"))
 
 fs.readFile('text-file.txt',()=>{
@@ -9,7 +13,7 @@ fs.readFile('text-file.txt',()=>{
 
     setTimeout(()=>console.log("timer 2 ...finished!"),0);
     
-    setTimeout(()=>console.log("timer 3 ...finished!"),3000);
+    setTimeout(()=>console.log("timer 3 ...finished!"),0);
 
     setImmediate(()=> console.log("Immediate 2 finished!"));
 
@@ -18,9 +22,19 @@ fs.readFile('text-file.txt',()=>{
 
     console.log('I/O finsihed!');
     
+    crypto.pbkdf2('password','salt', 100000,1024,'sha512', ()=>{
+        console.log(Date.now()-start,"Password encrypted!");   });
+    
+    crypto.pbkdf2('password','salt', 100000,1024,'sha512', ()=>{
+            console.log(Date.now()-start,"Password encrypted!");  }); 
+
+    crypto.pbkdf2('password','salt', 100000,1024,'sha512', ()=>{
+                console.log(Date.now()-start,"Password encrypted!");   });
+    crypto.pbkdf2('password','salt', 100000,1024,'sha512', ()=>{
+                    console.log(Date.now()-start,"Password encrypted!");  
+    });
 
 
-}
-)
+})
 
-console.log("hello from top!");  
+console.log("hello from top!");
